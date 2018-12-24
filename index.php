cd5f6f45ee78ee209c1870334af9f573e36ae3df
Frenzel Timothy <frenzel01@gmail.com>
2018-12-24 08:15:04 +0700

    login

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+  <head>
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+  </head>
+<body id="LoginForm">
+<div class="container">
+	
+<br><br>
+<h1 class="form-heading">Login</h1>
+<div class="login-form">
+<div class="main-div">
+    <div class="panel">
+   <p>Please enter your email and password</p>
+   </div>
+    <form id="Login">
+
+        <div class="form-group">
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email Address" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+
+    </form>
+	<form>
+	<button type="submit" class="btn btn-sm">Login as admin</button>
+	</form>
+    </div>
+</div></div></div>
+
+
+</body>
+</html>
