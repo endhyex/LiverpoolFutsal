9d0b86763cf91b76f5cf25159f958e11464f8d0f
Frenzel Timothy <frenzel01@gmail.com>
2018-12-24 09:03:38 +0700

    update

---
+++ adminHome.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Liverpool Futsal - Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="#" style="margin-left:20px">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="margin-left:40px">Reservation</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="margin-left:40px">Transaction</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item active">
+      <a class="text-info" style="margin-right:10px">Logged in as Administrator</a>
+  </ul>
+</nav>
+<div class="container">
+<br><br>
+<h1 class="text-dark text-center">Welcome, Administrator!</h1>
+</div>
+
+</body>
+</html>
